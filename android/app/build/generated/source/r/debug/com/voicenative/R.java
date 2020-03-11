33c3ed8856db4f40aced1a528c44d2779e9d51a4
hamza1220 <40993049+hamza1220@users.noreply.github.com>
2020-03-11 12:25:21 +0500

    APK_generation_ready

--- android/app/build/generated/source/r/debug/com/voicenative/R.java
+++ android/app/build/generated/source/r/debug/com/voicenative/R.java
@@ -1837,7 +1837,11 @@ containing a value of this type.
         public static final int abc_textfield_search_activated_mtrl_alpha=0x7f020042;
         public static final int abc_textfield_search_default_mtrl_alpha=0x7f020043;
         public static final int abc_textfield_search_material=0x7f020044;
-        public static final int notification_template_icon_bg=0x7f020045;
+        public static final int cancel=0x7f020045;
+        public static final int destroy=0x7f020046;
+        public static final int mic=0x7f020047;
+        public static final int notification_template_icon_bg=0x7f020049;
+        public static final int pause=0x7f020048;
     }
     public static final class id {
         public static final int action0=0x7f0d005b;
